4613aeaf811bd2db69f29cc7c415d087c0be1fb7
Pardha-vasamsety <109412879+Pardha-vasamsety@users.noreply.github.com>
2023-07-25 21:07:56 +0530

    Role based WEB API access using Azure AD authentication

---
+++ WebApiAdDemo/WebApiAdDemo/Controllers/ReportController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace WebApiAdDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [RequiredScope(RequiredScopesConfigurationKey ="AzureAd:scopes")]
+    public class ReportController : ControllerBase
+    {
+        [Authorize(Roles = "Manager")]
+        [HttpGet("[action]")]
+        public IActionResult GetReport()
+        {
+            return File(System.IO.File.ReadAllBytes("C:\\Explore1\\Azure-NET-Core-Apps\\Report.pdf"), "appliction/pdf");
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult GetReportStatus()
+        {
+            return Ok(new { status = @"Report generated at -" + DateTime.Now.ToString("MM-dd-YYYY HH:mm:ss") });
+        }
+    }
+}
